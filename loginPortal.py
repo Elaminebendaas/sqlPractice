f536e9d619334a5a81b13ff3dc3b8705bf3399c6
elamine <33202205+Elaminebendaas@users.noreply.github.com>
2022-04-17 00:19:12 -0400

    Add files via upload
    created verification to see if username already exists
    and fixed some small bugs left over from the other file

--- loginPortal.py
+++ loginPortal.py
@@ -1,30 +1,61 @@
 import sqlite3
 
-
-
-def dbInit():
-    conn.execute('''
-        CREATE TABLE USERS
-        (USERID INT PRIMARY KEY,
-        USERNAME TEXT NOT NULL,
-        PASSWORD TEXT NOT NULL);''')
-
-
-def accountCreation():
-    print('For account creation please input a desired username of password below:\n')
-    username = input("Enter your desired username: ")
-    password = input("Enter your desired password: ")
-    conn.execute("INSERT INTO USERS (%s, %s)", (username, password))
-    users = c.fetchall()
-    print(users)
-
-
-
-
 conn = sqlite3.connect('users.db')
 c = conn.execute("SELECT * FROM users")
 
 
-accountCreation()
+
+#Run this once so that the TABLE which stores the users credentials is initialized
+def dbInit():
+    conn.execute('''
+        CREATE TABLE USERS
+        (USERNAME TEXT NOT NULL,
+        PASSWORD TEXT NOT NULL);''')
 
 
+def accountCreation():
+    creationLoop = False
+    while creationLoop == False:
+        print('For account creation please input a desired username of password below:\n')
+        username = input("Enter your desired username: ")
+        password = input("Enter your desired password: ")
+        if existanceVerification(username) ==  True:  
+            conn.execute("INSERT INTO USERS VALUES(?, ?)",
+            (username, password))
+            print('Username already taken, enter a new one.')
+        else:
+            print("it worked?")
+            creationLoop = True
+        
+
+
+
+
+def existanceVerification(defUsername):
+    checker = False
+    users = c.fetchall()
+    user = str(defUsername)
+    for elem in users:
+        for usa in elem:
+            if usa == user:
+                checker = True
+                if checker == True:
+                    print("System has recongized another user with the same username. Select another one")
+                    return True
+                else:
+                    print('All clear!')
+                    return False
+
+    
+
+
+    
+
+users = c.fetchall()
+user = 'elamine'
+print(users)
+
+if existanceVerification(user) == True:
+    print('wtf')
+
+        
